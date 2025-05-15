abc4edf4af27de16e26daf95de40fa93af5c5408
et402 <ethee.tran@gmail.com>
2025-05-15 16:02:41 -0400

    Reapply "Merge branch 'main' into Ariel's_Branch"
    This reverts commit baa1f0e32711ec8ab57085a78d6954e218805a71.

--- ReignOfRuin/Assets/Scripts/Unit_System/States/CharacterStation.cs
+++ ReignOfRuin/Assets/Scripts/Unit_System/States/CharacterStation.cs
@@ -4,10 +4,14 @@ using System.Collections.Generic;
 
 public class CharacterStation : MonoBehaviour, UnitInterface
 { 
+   
+   public Transform stationCamTransform;
    public GameObject dialogueUI;
    public Dialogue dialogue;
-   private GameObject dialogueObj; 
+   //private GameObject dialogueObj; 
    private UnitHandler unitHandler;
+   [SerializeField]
+   private Bounds bounds;
 
    GameObject canvas;  
 
@@ -15,37 +19,58 @@ public class CharacterStation : MonoBehaviour, UnitInterface
    { 
       unitHandler = transform.parent.gameObject.GetComponent<UnitHandler>(); 
 
-      Again();
+      //Again();
       canvas = GameObject.Find("Canvas");  
+      
    }  
 
    public void Again()
    {
-      transform.parent.gameObject.tag = "Untagged";   
+      transform.parent.gameObject.tag = "Untagged"; 
+
+      if (transform.parent.gameObject.GetComponent<BoxCollider>() != null)
+         bounds = transform.parent.gameObject.GetComponent<BoxCollider>().bounds;
+      
+      if (dialogueUI == null)
+         dialogueUI = GameObject.Find("InteractionUI").transform.GetChild(0).gameObject;
    }
 
    private void OnTriggerEnter(Collider other)
-   {    
-      if (other.tag == "Player" && !PlayerStates._Instance.isEngaged && !unitHandler.imEngaged) { 
-         transform.parent.gameObject.tag = "Station"; 
-         DialogueEngaged();          
+   {
+      if (other.tag == "Player" && !PlayerStates._Instance.isEngaged)
+      {
+         transform.parent.gameObject.tag = "Station";
+         unitHandler.cameraZoomManager.StopFollowingPlayer();
+         unitHandler.cameraZoomManager.MoveToTarget(stationCamTransform);
+         DialogueEngaged();    
+               
       } 
    }  
 
    private void OnTriggerExit(Collider other)
    {
       if (other.tag == "Player") {
+         unitHandler.cameraZoomManager.FollowPlayerYOnly();
+         //cameraZoomManager.ResetCamera();
          Again();
-         Destroy(dialogueObj);
+         //Destroy(dialogueObj);
+         dialogueUI.SetActive(false);
          PlayerStates._Instance.isEngaged = false;
          unitHandler.imEngaged = false;
       }
    }  
 
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space) && unitHandler.imEngaged)
+         DialogueHandler._Instance.SpeechProceed();
+   }
+
    private void DialogueEngaged()
    {
       if (GameObject.FindWithTag("InteractUI") == null) {
-         dialogueObj = Instantiate(dialogueUI, canvas.transform.position, dialogueUI.transform.rotation, canvas.transform); 
+         //dialogueObj = Instantiate(dialogueUI, canvas.transform.position, dialogueUI.transform.rotation, canvas.transform); 
+         dialogueUI.SetActive(true);
          DialogueHandler._Instance.Begin(dialogue); 
          PlayerStates._Instance.isEngaged = true;
          unitHandler.imEngaged = true;
@@ -55,6 +80,7 @@ public class CharacterStation : MonoBehaviour, UnitInterface
    public void DestroyUI()
    {  
       PlayerStates._Instance.isEngaged = false; 
-      Destroy(dialogueObj);  
+      //Destroy(dialogueObj);  
+      dialogueUI.SetActive(false);
    }
 }
