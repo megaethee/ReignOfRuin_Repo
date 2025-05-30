bc0c7aefc69236b0e68abe4c6d0a7d12db8df543
SIM-Reaper <cb3557@drexel.edu>
2025-05-30 18:26:56 -0400

    Fixed Too Fast Text Bug

--- ReignOfRuin/Assets/Scripts/Dialogue_System/DialogueHandler.cs
+++ ReignOfRuin/Assets/Scripts/Dialogue_System/DialogueHandler.cs
@@ -19,6 +19,9 @@ public class DialogueHandler : MonoBehaviour
 
     public bool stopWriting;
 
+    private Coroutine typingCoroutine;
+    private bool isTyping = false;
+
     private void Awake()
     {
         if (null == _Instance)
@@ -40,25 +43,35 @@ public class DialogueHandler : MonoBehaviour
 
         testDialogue.inx = 0;        
         StartCoroutine(TypeWriter(testDialogue.dialogueSequence[testDialogue.inx], testDialogue));
-    } 
+    }
 
     public void SpeechProceed()
     {
-        testDialogue.inx++;
-        
-        if (testDialogue.inx >= testDialogue.dialogueSequence.Capacity) {
-            pB.CompleteProceedButton(); 
+        if (isTyping)
+        {
+            //Finish the line instantly
+            stopWriting = true;
             return;
         }
-        else  
-            if (testDialogue.dialogueSequence[testDialogue.inx] == null) {
-                pB.CompleteProceedButton();
-                return; 
-            }
-            else {
-                stopWriting = true;
-                StartCoroutine(TypeWriter(testDialogue.dialogueSequence[testDialogue.inx], testDialogue)); 
-            }
+
+        testDialogue.inx++;
+
+        if (testDialogue.inx >= testDialogue.dialogueSequence.Count)
+        {
+            pB.CompleteProceedButton();
+            return;
+        }
+
+        if (testDialogue.dialogueSequence[testDialogue.inx] == null)
+        {
+            pB.CompleteProceedButton();
+            return;
+        }
+
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        typingCoroutine = StartCoroutine(TypeWriter(testDialogue.dialogueSequence[testDialogue.inx], testDialogue));
     }
 
     // Update is called once per frame
@@ -68,15 +81,29 @@ public class DialogueHandler : MonoBehaviour
     //        SpeechProceed();
     //}
 
-//put an interrupt here so its not janky
+    //put an interrupt here so its not janky
     public IEnumerator TypeWriter(string dialogue, Dialogue testDialogue) {
         stopWriting = false;
-        int curInx = testDialogue.inx; 
-        for (int i=0; i<testDialogue.dialogueSequence[curInx].Length+1; i++) {
-            txtToScreen.text = testDialogue.dialogueSequence[curInx].Substring(0, i);
+        isTyping = true;
+        int curInx = testDialogue.inx;
+        string fullText = testDialogue.dialogueSequence[curInx];
+        txtToScreen.text = "";
+
+        for (int i = 0; i <= fullText.Length; i++)
+        {
+            if (stopWriting)
+            {
+                txtToScreen.text = fullText; // Immediately reveal full text
+                break;
+            }
+
+            txtToScreen.text = fullText.Substring(0, i);
 
             if (stopWriting) yield break;
             yield return new WaitForSeconds(delay);
         }
+
+        stopWriting = false;
+        isTyping = false;
     }
 }
